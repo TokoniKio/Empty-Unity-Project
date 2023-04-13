7f6097045420bc332484fea9e64eec1cd75e4aa2
Tokoni Kio <127438391+TokoniKio@users.noreply.github.com>
2023-04-13 10:59:55 -0700

    Update PlayerController.cs

--- Evil Cave/Assets/Scripts/PlayerController.cs
+++ Evil Cave/Assets/Scripts/PlayerController.cs
@@ -17,8 +17,13 @@ public class PlayerController : MonoBehaviour
     private Coroutine pickaxeDefaultCountdown;
     public float strength = 15.0f;
     private int lives;
+    [SerializeField] private Rigidbody _rb;
+    [SerializeField] private float _speed = 5;
+    [SerializeField] private float _turnSpeed = 360;
+    private Vector3 _input;
     
     // Start is called before the first frame update
+
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
@@ -28,42 +33,54 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-
-        verticalInput = Input.GetAxis("Vertical");
-
-        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
-        movementDirection.Normalize();
-        transform.Translate(movementDirection * playerMoveSpeed * Time.deltaTime, Space.World);
-
-        if(movementDirection != Vector3.zero)
-        {
-            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
-
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
-        }
+        GatherInput();
+        Look();
 
         if(Input.GetMouseButtonDown(0))
         {
-            {
                 if(isCoolDown == false)
-                {
+            {
                     Debug.Log("Pickaxe Used");
                     isAttacking = true;
                     pickaxe.transform.Rotate(Vector3.forward, 50.0f);
                     pickaxeDefaultCountdown = StartCoroutine(PickaxeCountdown());
-                }
             }
         }
+    }
 
-        IEnumerator PickaxeCountdown()
-        {
+    IEnumerator PickaxeCountdown()
+    {
         isCoolDown = true;
-            yield return new WaitForSeconds(.3f);
-            isAttacking = false;
+        yield return new WaitForSeconds(.3f);
+        isAttacking = false;
         pickaxe.transform.Rotate(Vector3.forward, -50.0f);
         isCoolDown = false;
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void GatherInput()
+    {
+        _input = new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical"));
+    }
+
+    void Look()
+    {
+        if(_input != Vector3.zero)
+        {
+            var relative = (transform.position + _input) - transform.position;
+            var rot = Quaternion.LookRotation(relative,Vector3.up);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation,rot,_turnSpeed * Time.deltaTime);
         }
+    }
+
+    void Move()
+    {
+        _rb.MovePosition(transform.position + (transform.forward * _input.magnitude) * _speed * Time.deltaTime);
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -72,10 +89,10 @@ public class PlayerController : MonoBehaviour
 
         if (collision.gameObject.CompareTag("Enemy") && !isAttacking)
         {
-            Rigidbody enemyRb = collision.gameObject.GetComponent<Rigidbody>();
-            Vector3 awayFromPlayer = collision.gameObject.transform.position - playerRb.transform.position;
+            //Rigidbody enemyRb = collision.gameObject.GetComponent<Rigidbody>();
+            //Vector3 awayFromPlayer = collision.gameObject.transform.position - playerRb.transform.position;
 
-            enemyRb.AddForce(awayFromPlayer * strength, ForceMode.Impulse);
+            //enemyRb.AddForce(awayFromPlayer * strength, ForceMode.Impulse);
             UpdateLives(-1);
         }
     }
