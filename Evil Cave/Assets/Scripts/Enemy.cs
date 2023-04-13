b26760b19cb2127da2823acae89388ec17300d5a
Tokoni Kio <127438391+TokoniKio@users.noreply.github.com>
2023-04-13 12:23:41 -0700

    TMP

--- Evil Cave/Assets/Scripts/Enemy.cs
+++ Evil Cave/Assets/Scripts/Enemy.cs
@@ -2,17 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
+using TMPro;
+
 
 public class Enemy : MonoBehaviour
 {
     public float enemyMoveSpeed = 5.0f;
+    private Rigidbody enemyRb;
     private GameObject player;
     private PlayerController playerController;
-    public int health = 5;
-
-    [SerializeField] private Rigidbody _enemyRb;
-    [SerializeField] private float _enemySpeed = 5;
-    [SerializeField] private float _enemyTurnSpeed = 360;
+    public int health;
+    public TextMeshProUGUI healthText;
 
     public Transform target;
     [SerializeField]
@@ -20,51 +21,31 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        enemyRb = GetComponent<Rigidbody>();
         player = GameObject.Find("Player");
-        playerController = player.GetComponent<PlayerController>();
+        playerController = GameObject.Find("Player").GetComponent<PlayerController>();
+        UpdateHealth(5);
     }
 
     // Update is called once per frame
     void Update()
-    {
-        Look();
-
+    {        
         Vector3 lookDirection = (player.transform.position - transform.position).normalized;
         
-        _enemyRb.AddForce(lookDirection * _enemySpeed);
+        enemyRb.AddForce(lookDirection * enemyMoveSpeed);
 
         Vector3 relativePos = target.position - transform.position;
         Quaternion rotation = Quaternion.LookRotation(relativePos);
 
         Quaternion current = transform.localRotation;
 
-        transform.localRotation = Quaternion.Slerp(current, rotation, Time.deltaTime * _enemySpeed);
-    }
-
-    void FixedUpdate()
-    {
-        Move();
-    }
-
-    void Look()
-    {
-        if(playerController._input != Vector3.zero)
-        {
-            var relative = (transform.position + playerController._input) - transform.position;
-            var rot = Quaternion.LookRotation(relative,Vector3.up);
-
-            transform.rotation = Quaternion.RotateTowards(transform.rotation,rot,_enemyTurnSpeed * Time.deltaTime);
-        }
-    }
-
-    void Move()
-    {
-        _enemyRb.MovePosition(transform.position + (transform.forward * playerController._input.magnitude) * _enemySpeed * Time.deltaTime);
+        transform.localRotation = Quaternion.Slerp(current, rotation, Time.deltaTime * enemyMoveSpeed);
     }
 
     public void UpdateHealth(int healthToChange)
     {
         health+= healthToChange;
+        healthText.text = "Health: " + health;
         if (health <= 0)
         {
             Destroy(gameObject);
